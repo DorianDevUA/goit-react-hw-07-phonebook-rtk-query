aa4582ea364a924c5d0f8c4a348babc546b29f28
Dorian Devua <Dorian.DevUa@gmail.com>
2024-07-13 16:05:45 +0400

    added handling of HTTP request errors

--- src/redux/operationsThunk.js
+++ src/redux/operationsThunk.js
@@ -5,8 +5,12 @@ axios.defaults.baseURL = 'https://669013f3c0a7969efd9ae577.mockapi.io/api/v1';
 
 export const fetchAllContactsThunk = createAsyncThunk(
   'contacts/fetchAll',
-  async () => {
-    const response = await axios.get('/contacts');
-    return response.data;
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   },
 );
